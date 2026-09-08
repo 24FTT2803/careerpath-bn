dc8e05ce02d4f473ec0371b2441d254f63d4c955
Syafiq <24ftt1802@student.pb.edu.bn>
2026-09-09 01:40:50 +0800

    fix(user): cast last active timestamp

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,6 +47,7 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
             'last_login_at' => 'datetime',
+            'last_active_at' => 'datetime',
             'show_ads' => 'boolean',
         ];
     }
